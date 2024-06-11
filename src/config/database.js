25d5821ca18852c4788879c4e861008a29bf251a
Limber Jhonathan <limberjhonathan@gmail.com>
2024-06-10 22:40:22 -0300

    Removendo o console log

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,5 @@
 require('dotenv').config({ path: '/home/jhonathan/api/dist/.env' });
 
-console.log('DATABASE_HOST:', process.env.DATABASE_HOST);
-console.log('DATABASE_PORT:', process.env.DATABASE_PORT);
-console.log('DATABASE_USERNAME:', process.env.DATABASE_USERNAME);
-console.log('DATABASE_PASSWORD:', process.env.DATABASE_PASSWORD);
-console.log('DATABASE:', process.env.DATABASE);
-
 module.exports = {
   dialect: 'mariadb',
   host: process.env.DATABASE_HOST,
