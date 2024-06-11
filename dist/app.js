eff46d60adc0cdc7036576a42c638cd70a304045
Limber Jhonathan <limberjhonathan@gmail.com>
2024-06-10 21:45:40 -0300

    Alterações

--- dist/app.js
+++ dist/app.js
@@ -6,6 +6,9 @@ _dotenv2.default.config();
 require('./database');
 
 var _express = require('express'); var _express2 = _interopRequireDefault(_express);
+// import cors from 'cors';
+// import helmet from 'helmet';
+
 var _homeRoutes = require('./routes/homeRoutes'); var _homeRoutes2 = _interopRequireDefault(_homeRoutes);
 var _userRoutes = require('./routes/userRoutes'); var _userRoutes2 = _interopRequireDefault(_userRoutes);
 var _tokenRoutes = require('./routes/tokenRoutes'); var _tokenRoutes2 = _interopRequireDefault(_tokenRoutes);
