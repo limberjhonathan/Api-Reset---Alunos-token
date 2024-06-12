1017e302a8af6417478d25de0e6b69ed9b4458be
Limber Jhonathan <limberjhonathan@gmail.com>
2024-06-12 20:26:36 -0300

    adicionando arquivos

--- dist/app.js
+++ dist/app.js
@@ -17,7 +17,7 @@ var _fotoRoutes = require('./routes/fotoRoutes'); var _fotoRoutes2 = _interopReq
 
 const whiteList = [
   'http://localhost:3000',
-  'https://storiesig.info/en/',
+  'https://pt.wikipedia.org',
 ];
 
 const corsOptions = {
--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ import fotoRoutes from './routes/fotoRoutes';
 
 const whiteList = [
   'http://localhost:3000',
-  'https://storiesig.info/en/',
+  'https://pt.wikipedia.org',
 ];
 
 const corsOptions = {
